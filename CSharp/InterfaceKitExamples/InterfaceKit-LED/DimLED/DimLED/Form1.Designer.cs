03ad65077511eefa11733861b61b751c2ccff59e
Nicco Kunzmann <niccokunzmann@student.hpi.uni-potsdam.de>
2015-09-15 16:50:06 +0200

    added LED interface kit example

---
+++ CSharp/InterfaceKitExamples/InterfaceKit-LED/DimLED/DimLED/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace DimLED
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(98, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "sensor_value1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(56, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "led1_value";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Sensor Value 1";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 53);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "LED0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 66);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "LED1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(57, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "led0_value";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(190, 124);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ CSharp/InterfaceKitExamples/InterfaceKit-LED/DimLED/DimLED/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//Needed for the InterfaceKit class, phidget base classes, and the PhidgetException class
+using Phidgets;
+//Needed for the event handling classes 
+using Phidgets.Events;  
+
+
+namespace DimLED
+{
+    public partial class Form1 : Form
+    {
+
+        static InterfaceKit ifKit;
+
+        int sensor1_value = 0;
+
+        Random random;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            random = new Random();
+
+            ifKit = new InterfaceKit();
+
+            //Hook the basica event handlers
+            ifKit.Attach += new AttachEventHandler(ifKit_Attach);
+            ifKit.Detach += new DetachEventHandler(ifKit_Detach);
+            
+            //Hook the phidget spcific event handlers
+            ifKit.SensorChange += new SensorChangeEventHandler(ifKit_SensorChange);
+
+            //Open the object for device connections
+            ifKit.open(); 
+            
+        }
+
+        //Attach event handler...Display the serial number of the attached InterfaceKit 
+        //to the console
+        void ifKit_Attach(object sender, AttachEventArgs e)
+        {
+            this.Text = "attached";
+            // start the timer
+            this.timer1.Start();
+        }
+
+        //Detach event handler...Display the serial number of the detached InterfaceKit 
+        //to the console
+        void ifKit_Detach(object sender, DetachEventArgs e)
+        {
+            this.Text = "detached";
+            // stop the timer
+            this.timer1.Stop();
+        }
+
+        //Sensor Change event handler...Display the sensor index and it's new value to 
+        //the console
+        void ifKit_SensorChange(object sender, SensorChangeEventArgs e)
+        {
+            if (e.Index == 0)
+            {
+                this.label1.Text = e.Value.ToString();
+                sensor1_value = e.Value;
+                bool on = sensor1_value > 500;
+                ifKit.outputs[0] = on;
+                label6.Text = on.ToString();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            bool on = random.Next(1000) < sensor1_value;
+            ifKit.outputs[1] = on;
+            label2.Text = on.ToString();
+        }
+
+    }
+}
